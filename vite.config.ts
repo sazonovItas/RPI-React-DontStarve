b182b7e65d58220ce602cef239980d65179ddc6f
sazonovItas <itassudo@gmail.com>
2023-12-04 22:39:31 +0300

    fix useContext

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RPI-React-DontStarve",
+  base: "/RPI-React-DontStarve/",
   resolve: {
     alias: {
       src: "/src",
